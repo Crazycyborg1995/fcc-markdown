b9398f27729623694b414eb26e297b76c411f42f
Crazycyborg1995 <mhdafsalvp@gmail.com>
2019-02-07 20:57:26 +0530

    Updates

---
+++ precache-manifest.f3890b0a28bd2a5b4a540abad6a9ddb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef078a0ef438cc6db7d7",
+    "url": "/fcc-markdown/static/css/main.11fdd0b1.chunk.css"
+  },
+  {
+    "revision": "ef078a0ef438cc6db7d7",
+    "url": "/fcc-markdown/static/js/main.ef078a0e.chunk.js"
+  },
+  {
+    "revision": "73298c12ab245af03e8d",
+    "url": "/fcc-markdown/static/js/1.73298c12.chunk.js"
+  },
+  {
+    "revision": "a432972f16cbe49ce385",
+    "url": "/fcc-markdown/static/js/runtime~main.a432972f.js"
+  },
+  {
+    "revision": "01a5ac68867a643ccf451a32521282fa",
+    "url": "/fcc-markdown/index.html"
+  }
+];
